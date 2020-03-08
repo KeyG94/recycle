f84ee7387c03f0d5a3dc7e14942483843c5bcdbc
Glenn Key <glennkey78@live.no>
2020-03-08 22:10:02 +0100

    added a script making content on the page slide in when they appear on scroll

---
+++ js/animate.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.col2, .headImg, .rollImg', {
+    origin: 'bottom',
+    distance: '75px',
+    duration: 1000,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
