6605887f981325d4950198290c233987b4ab8c0e
Glenn Key <glennkey78@live.no>
2020-03-09 13:31:55 +0100

    Updated the sliding script to target all html sites; updated the phone version so that the sliding effect didnt cause un-expected results; added meta description etc. to the head; ready to merge

--- js/animate.js
+++ js/animate.js
@@ -1,9 +1,14 @@
 ScrollReveal().reveal('.col2, .headImg, .rollImg', {
     origin: 'bottom',
-    distance: '75px',
+    distance: '66px',
     duration: 1000,
     easing: 'ease',
     mobile: true,
     reset: true,
     viewFactor: 0.2,
+});
+
+ScrollReveal().reveal('iframe',{
+    duration: 2000,
+    origin: 'bottom',
 });
